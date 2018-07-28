1a10d6cb93f51ab5d67ab4c09c44b5b88ae7e8fa
Viktor Ivanov <v.kr.ivanov@gmail.com>
2018-07-28 17:10:10 +0300

    Created OfferService interface

--- manager/src/main/java/org/merchantservices/manager/service/OfferService.java
+++ manager/src/main/java/org/merchantservices/manager/service/OfferService.java
@@ -1,60 +1,18 @@
 package org.merchantservices.manager.service;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import org.merchantservices.manager.entity.Offer;
-import org.merchantservices.manager.exception.OfferCancelledException;
-import org.merchantservices.manager.exception.OfferExpiredException;
-import org.merchantservices.manager.exception.OfferNotFoundException;
-import org.merchantservices.manager.repository.OfferRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
-public class OfferService {
+public interface OfferService {
 
-	@Autowired
-	private OfferRepository offerRepository;
+	Offer getOffer(String id);
 
-	public Offer getOffer(String id) {
-		Optional<Offer> offer = offerRepository.findById(id);
+	List<Offer> getActiveOffers();
 
-		if (offer.isPresent()) {
-			return offer.get();
-		}
+	List<Offer> getArchivedOffers();
 
-		throw new OfferNotFoundException();
-	}
+	Offer saveOffer(Offer offer);
 
-	public List<Offer> getActiveOffers() {
-		return offerRepository.findByCancelDateIsNullAndExpiryDateGreaterThan(new Date());
-	}
-
-	public List<Offer> getArchivedOffers() {
-		return offerRepository.findByCancelDateIsNotNullOrExpiryDateLessThan(new Date());
-	}
-
-	public Offer saveOffer(Offer offer) {
-		offer.setCreationDate(new Date());
-
-		return offerRepository.save(offer);
-	}
-
-	public Offer cancelOffer(String id) {
-		Offer offer = getOffer(id);
-
-		if (offer.getCancelDate() != null) {
-			throw new OfferCancelledException();
-		}
-
-		if (offer.getExpiryDate().before(new Date())) {
-			throw new OfferExpiredException();
-		}
-
-		offer.setCancelDate(new Date());
-
-		return offerRepository.save(offer);
-	}
+	Offer cancelOffer(String id);
 }
---
+++ manager/src/main/java/org/merchantservices/manager/service/OfferServiceImpl.java
@@ -0,0 +1,65 @@
+package org.merchantservices.manager.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.merchantservices.manager.entity.Offer;
+import org.merchantservices.manager.exception.OfferCancelledException;
+import org.merchantservices.manager.exception.OfferExpiredException;
+import org.merchantservices.manager.exception.OfferNotFoundException;
+import org.merchantservices.manager.repository.OfferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OfferServiceImpl implements OfferService {
+
+	@Autowired
+	private OfferRepository offerRepository;
+
+	@Override
+	public Offer getOffer(String id) {
+		Optional<Offer> offer = offerRepository.findById(id);
+
+		if (offer.isPresent()) {
+			return offer.get();
+		}
+
+		throw new OfferNotFoundException();
+	}
+
+	@Override
+	public List<Offer> getActiveOffers() {
+		return offerRepository.findByCancelDateIsNullAndExpiryDateGreaterThan(new Date());
+	}
+
+	@Override
+	public List<Offer> getArchivedOffers() {
+		return offerRepository.findByCancelDateIsNotNullOrExpiryDateLessThan(new Date());
+	}
+
+	@Override
+	public Offer saveOffer(Offer offer) {
+		offer.setCreationDate(new Date());
+
+		return offerRepository.save(offer);
+	}
+
+	@Override
+	public Offer cancelOffer(String id) {
+		Offer offer = getOffer(id);
+
+		if (offer.getCancelDate() != null) {
+			throw new OfferCancelledException();
+		}
+
+		if (offer.getExpiryDate().before(new Date())) {
+			throw new OfferExpiredException();
+		}
+
+		offer.setCancelDate(new Date());
+
+		return offerRepository.save(offer);
+	}
+}
