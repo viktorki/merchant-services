02d4d904771bd9607c2761f5d0968a079a66cd17
Viktor Ivanov <v.kr.ivanov@gmail.com>
2018-07-28 22:36:58 +0300

    Added OfferRepository Mongo tests

---
+++ manager/src/test/java/org/merchantservices/manager/repository/OfferRepositoryTest.java
@@ -0,0 +1,73 @@
+package org.merchantservices.manager.repository;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.merchantservices.manager.entity.Offer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class OfferRepositoryTest {
+
+	@Autowired
+	private MongoTemplate mongoTemplate;
+
+	@Autowired
+	private OfferRepository offerRepository;
+
+	@Before
+	public void before() {
+		final Calendar c = Calendar.getInstance();
+		c.add(Calendar.MONTH, -1);
+		final Date date1 = c.getTime();
+		c.add(Calendar.MONTH, 2);
+		final Date date2 = c.getTime();
+		final Offer offer1 = new Offer();
+		offer1.setTitle("Test title 1");
+		offer1.setContent("Test Content 1");
+		offer1.setCreationDate(new Date());
+		offer1.setExpiryDate(date1);
+		final Offer offer2 = new Offer();
+		offer2.setTitle("Test title 2");
+		offer2.setContent("Test Content 2");
+		offer2.setCreationDate(new Date());
+		offer2.setExpiryDate(date2);
+		final Offer offer3 = new Offer();
+		offer3.setTitle("Test title 3");
+		offer3.setContent("Test Content 3");
+		offer3.setCreationDate(new Date());
+		offer3.setExpiryDate(date2);
+		offer3.setCancelDate(new Date());
+
+		mongoTemplate.remove(Offer.class).all();
+		mongoTemplate.save(offer1);
+		mongoTemplate.save(offer2);
+		mongoTemplate.save(offer3);
+	}
+
+	@Test
+	public void testFindByCancelDateIsNullAndExpiryDateGreaterThan() {
+		List<Offer> offers = offerRepository.findByCancelDateIsNullAndExpiryDateGreaterThan(new Date());
+
+		Assert.assertEquals(1, offers.size());
+		Assert.assertEquals("Test title 2", offers.get(0).getTitle());
+	}
+
+	@Test
+	public void testFindByCancelDateIsNotNullOrExpiryDateLessThan() {
+		List<Offer> offers = offerRepository.findByCancelDateIsNotNullOrExpiryDateLessThan(new Date());
+
+		Assert.assertEquals(2, offers.size());
+		Assert.assertNotEquals("Test title 2", offers.get(0).getTitle());
+		Assert.assertNotEquals("Test title 2", offers.get(1).getTitle());
+	}
+}
